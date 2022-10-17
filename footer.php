e674c2a2f7b337aee098b3590a9de5b40604cfb1
Surendra Maurya <surendra75maurya@gmail.com>
2022-10-17 10:31:08 +0530

    Changes for other page nav

--- footer.php
+++ footer.php
@@ -43,7 +43,7 @@
           <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
             <!-- Content -->
             <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3"></i>Company name
+              <i class="fas fa-gem me-3"></i>DigitalCoTech
             </h6>
             <p>
               Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -115,8 +115,8 @@
   
     <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-      © 2021 Copyright:
-      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      © 2022 Copyright:
+      <a class="text-reset fw-bold" href="https://digitalcotech.com/">digitalcotech.com</a>
     </div>
     <!-- Copyright -->
   </footer>

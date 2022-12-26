088a16b1d1f725870e75eda58faa8a05614af47d
Surendra Maurya <surendra75maurya@gmail.com>
2022-12-26 20:14:23 +0530

    Changes for Database

--- free-udemy-courses.php
+++ free-udemy-courses.php
@@ -97,6 +97,97 @@
       </div>
     </div>
   </div>
+<div class="container  pt-4 pb-4 text-center ">
+   <h3 class="text-left" style="text-align: left;">Databases</h3>
+    <div class="row">
+        <!-- <h3 class="text-left" style="text-align: left;">Development</h3> -->
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+          <img src="udemy/2640080_492f_2.jpg"  class="card-img-top" alt="A Beginners Guide to SQL">
+          <div class="card-body">
+            <h6 class="card-title">A Beginners Guide to SQL</h6>
+            <p class="card-text">A gentle introduction to the world of SQL.</p>
+            <a href="https://udemyfreecourses.org/redirect/2640080/course/sql-101-a-beginners-guide-to-sql/" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2543659_d4e6_3.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">Oracle SQL: An Introduction to the most popular database</h6>
+            <p class="card-text">A quick tour of the six principal clauses of the SELECT statement.</p>
+            <a href="https://udemyfreecourses.org/redirect/2543659/course/oracle-sql-an-introduction-to-the-most-popular-database/" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/3771224_5d52_8.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">Learn MongoDB in 2 hours</h6>
+            <p class="card-text">Learn MongoDB in simple fastest manner using this beginner's tutorial containing basic to advanced knowledge</p>
+            <a href="https://www.udemy.com/course/getting-started-with-mongodb/" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/1913512_0d0c_4.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">Free SQL 101 Class Bootcamp Beginners Intro to SQL</h6>
+            <p class="card-text">Gentle Introduction to Non Programmers on SQL for Analytics. 1 day Bootcamp Course Begineers Hands-on. Project: New York</p>
+            <a href="https://www.udemy.com/course/sql-analytics-coding-101-bootcamp-begineers-course-hand-on/?LSNPUBID=PPe1bPYHvDA&ranEAID=PPe1bPYHvDA&ranMID=39197&ranSiteID=PPe1bPYHvDA-VGc2ux8YpwCZ0gUOOtIozg&utm_medium=udemyads&utm_source=aff-campaign" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<div class="container  pt-4 pb-4 text-center ">
+    <div class="row">
+        <!-- <h3 class="text-left" style="text-align: left;">Development</h3> -->
+        <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+          <div class="card" >
+            <img src="udemy/328976_8ad9_2.jpg"  class="card-img-top" alt="A Beginners Guide to SQL">
+            <div class="card-body">
+              <h6 class="card-title">Database Design and Management</h6>
+              <p class="card-text">Learn how to design and manage database with ERD, database generation and reversal with Visual Paradigm.</p>
+              <a href="https://www.udemy.com/course/database-design-and-management/?LSNPUBID=PPe1bPYHvDA&ranEAID=PPe1bPYHvDA&ranMID=39197&ranSiteID=PPe1bPYHvDA-eeLWIRGqhkQq0Ss0jf4hyg&utm_medium=udemyads&utm_source=aff-campaign" class="">Read More</a>
+            </div>
+          </div>
+        </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2276621_64c0.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">ClickHouse crash course. Conquer big data with ease</h6>
+            <p class="card-text">Learn how to use one of the most powerful open source OLAP database on the market. Put new life in your big data.</p>
+            <a href="https://udemyfreecourses.org/redirect/2276621/course/clickhouse-crash-course-conquer-big-data-with-ease/" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2094844_ec50_5.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">AWS Tutorials - DynamoDB and Database Migration Service</h6>
+            <p class="card-text">Hands on video showcasing AWS DynamoDB and Database Migration Service.</p>
+            <a href="https://www.udemy.com/course/namrata-h-shah-aws-tutorials-dynamodb-and-database-migration-service/?LSNPUBID=PPe1bPYHvDA&ranEAID=PPe1bPYHvDA&ranMID=39197&ranSiteID=PPe1bPYHvDA-PWAGB28dnr7TqG5AYAW8nw&utm_medium=udemyads&utm_source=aff-campaign" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/4649154_edaf.jpg"  class="card-img-top" alt="...">
+          <div class="card-body">
+          <h6 class="card-title">SQL to Python for Beginners</h6>
+            <p class="card-text">An intro Course illustrating SQL equivalents in Python.</p>
+            <a href="https://www.udemy.com/course/sql-to-python-for-beginners/?LSNPUBID=PPe1bPYHvDA&ranEAID=PPe1bPYHvDA&ranMID=39197&ranSiteID=PPe1bPYHvDA-5CZzqb2TxAg2WEpS7qUUPA&utm_medium=udemyads&utm_source=aff-campaign" class="">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- <div class="container  pt-4 pb-4 ">
     <div class="row">

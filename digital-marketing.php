4697f5d4ea02cc5b02b5d19917adc5d682198dc5
Surendra Maurya <surendra75maurya@gmail.com>
2023-01-04 08:26:07 +0530

    Chnages Added course

---
+++ digital-marketing.php
@@ -0,0 +1,248 @@
+<?php 
+$title = "Free Digital Marketing Tutorials on Udemy";
+$ogtitle = "Free Digital Marketing Tutorials on Udemy";
+$ogdesc = "Learn the fundamentals of programming from a top-rated Udemy instructor. Whether you're interested in learning programming languages like C, PHP, HTML, and C#, or concepts like Object Oriented Programming, Udemy has a course to help you become a master coder.";
+$ogurl = "https://www.digitalcotech.com/digital-marketing.php";
+$ogimg = "https://www.digitalcotech.com/udemy/1053706_c51c_7.jpg";
+include('header.php');
+
+?>
+<div class="alert alert-warning alert-dismissible fade show text-center mb-0" role="alert" style="border-radius: unset;">
+  <p class="mb-0"><strong>Thank you for contacting <b class="alert">DigitalCoTech..</b><strong></p>
+</div>
+
+    
+<div class="container  p-5 text-center ">
+    <h4 class="mb-3" style="background-color: rgba(0, 0, 0, 0.3);">Free Digital Marketing Tutorials on Udemy</h4>
+    <div class="row">   
+        <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <div class="card" >
+              <img src="udemy/1716470_63ab_3.jpg"  class="card-img-top" alt="...">
+              <div class="card-body">
+                <h6 class="card-title">Free Digital Marketing Basics Course</h6>
+                <p class="card-text">Become Great at Digital Marketing</p>
+                <a href="https://www.udemy.com/course/free-digital-marketing-basics-course/" class="btn btn-warning">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <div class="card" >
+            <img src="udemy/546480_a312_6.jpg"  class="card-img-top" alt="...">
+              <div class="card-body">
+              <h6 class="card-title">Digital Marketing Fundamentals with Live Projects</h6>
+                <p class="card-text">Jump Start Your Career Growth & Earning Potential. Learn Digital Marketing from Global Experts.</p>
+                <a href="https://www.udemy.com/course/digital-marketing-foundation/" class="btn btn-warning">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <div class="card" >
+            <img src="udemy/875494_7139.jpg"  class="card-img-top" alt="...">
+              <div class="card-body">
+              <h6 class="card-title">Email Marketing Made Easy For Beginners</h6>
+                <p class="card-text">Learn The Basics of Creating Effective Email Marketing Campaigns That Make Your Business More Successful</p>
+                <a href="https://www.udemy.com/course/email-marketing-made-easy-for-beginners/" class="btn btn-warning">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <div class="card" >
+            <img src="udemy/881700_6ce1_3.jpg"  class="card-img-top" alt="...">
+              <div class="card-body">
+              <h6 class="card-title">MailChimp for Beginners (1 hour starter course)</h6>
+                <p class="card-text">Learn the basics of MailChimp and email marketing in under an hour</p>
+                <a href="https://www.udemy.com/course/mailchimp-starter-course/" class="btn btn-warning">Read More</a>
+              </div>
+            </div>
+          </div>
+    </div>
+  </div>
+<div class="container  p-5 text-center ">
+    <div class="row">   
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+          <img src="udemy/2463482_bb29_2.jpg"  class="card-img-top" alt="HTML5 and CSS3">
+          <div class="card-body">
+            <h6 class="card-title">Online Digital Social Media Marketing & Sales Free Training</h6>
+            <p class="card-text">Free Training - Learn how to master marketing by understanding the buyer journey and how the buyer thinks and buys.</p>
+         
+            <a href="https://www.udemy.com/course/online-digital-social-media-marketing-sales-for-beginners/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2709080_365e_2.jpg"  class="card-img-top" alt="Javascript Essentials">
+          <div class="card-body">
+          <h6 class="card-title">Digital Marketing Leads Generator</h6>
+            <p class="card-text">Lets Invest in Your Business and Take It to the Next Level</p>
+            <a href="https://www.udemy.com/course/empirethoughts-digital-leads-generator/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/4633954_2338.jpg"  class="card-img-top" alt="C++ Tutorial for Complete Beginners">
+          <div class="card-body">
+          <h6 class="card-title">React JS Crash Course For Beginners [2022 Updated]</h6>
+            <p class="card-text">Learn all core features in React JS</p>
+            <a href="https://www.udemy.com/course/the-complete-react-js-developer-course-for-beginners-2022/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/519196_1746_5.jpg"  class="card-img-top" alt="Introduction To Python Programming">
+          <div class="card-body">
+          <h6 class="card-title">YouTube Thumbnails Power of Images for SEO Video Marketing</h6>
+            <p class="card-text">Create Powerful Thumbnails to DRIVE More Traffic to your Video. How to optimize your Videos for better SEO in Search</p>
+            <a href="https://www.udemy.com/course/youtube-thumbnails-power-of-images-for-seo-video-marketing/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<div class="container  p-5 text-center ">
+    <div class="row">   
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+          <img src="udemy/4157788_e738_3.jpg"  class="card-img-top" alt="HTML5 and CSS3">
+          <div class="card-body">
+            <h6 class="card-title">Instagram Masterclass: Instagram Marketing</h6>
+            <p class="card-text">Discover tried, tested & result-driven Instagram Marketing strategies for ensuring Instagram growth.</p>
+         
+            <a href="https://www.udemy.com/course/instagram-marketing-101/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/1718196_24ba.jpg"  class="card-img-top" alt="Javascript Essentials">
+          <div class="card-body">
+          <h6 class="card-title">Applied Marketing Strategies In The Digital Era</h6>
+            <p class="card-text">Adapt your company to the evolving marketing environment</p>
+            <a href="https://www.udemy.com/course/applied-marketing-strategies-in-the-digital-era/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/4157802_3e22_4.jpg"  class="card-img-top" alt="C++ Tutorial for Complete Beginners">
+          <div class="card-body">
+          <h6 class="card-title">LinkedIn Masterclass: LinkedIn Marketing </h6>
+            <p class="card-text">Discover effective LinkedIn Marketing strategies, build an All-Star profile & network with industry experts</p>
+            <a href="https://www.udemy.com/course/linkedin-marketing-101/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2008564_9a61_3.jpg"  class="card-img-top" alt="Introduction To Python Programming">
+          <div class="card-body">
+          <h6 class="card-title">Digital Media and Marketing</h6>
+            <p class="card-text">Combination of digital media and sports marketing</p>
+            <a href="https://www.udemy.com/course/digital-media-and-marketing/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<div class="container  p-5 text-center ">
+    <div class="row">   
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+          <img src="udemy/2417574_658b_4.jpg"  class="card-img-top" alt="HTML5 and CSS3">
+          <div class="card-body">
+            <h6 class="card-title">How to Start a Blog</h6>
+            <p class="card-text">A Beginners Guide to Starting a WordPress Blog in 30 Minutes or LESS!</p>
+         
+            <a href="https://www.udemy.com/course/how-to-start-a-blog-for-beginners/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/3626618_91de.jpg"  class="card-img-top" alt="Javascript Essentials">
+          <div class="card-body">
+          <h6 class="card-title">Digital Marketing for Websites: Basics of SEO & Analytics</h6>
+            <p class="card-text">Learn how to make sense of website statistics & increase organic traffic. A course by Visitor Analytics</p>
+            <a href="https://www.udemy.com/course/digital-marketing-for-websites-basics-of-seo-and-analytics/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/2573174_997c_3.jpg"  class="card-img-top" alt="C++ Tutorial for Complete Beginners">
+          <div class="card-body">
+          <h6 class="card-title">Hospitality Inbound Marketing Fundamentals and practices</h6>
+            <p class="card-text">Very Basics of Digital marketing (Inbound Part), Steps & methods used, Content Creation, Social Media Strategies</p>
+            <a href="https://www.udemy.com/course/hospitality-inbound-marketing-fundamentals/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/670890_b6b1_2.jpg"  class="card-img-top" alt="Introduction To Python Programming">
+          <div class="card-body">
+          <h6 class="card-title">Organic Search And Keywords</h6>
+            <p class="card-text">Learn how to integrate marketing requirements with a technique</p>
+            <a href="https://www.udemy.com/course/seo-keywords/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<div class="container  p-5 text-center ">
+    <div class="row">   
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+          <img src="udemy/3181450_4928_3.jpg"  class="card-img-top" alt="HTML5 and CSS3">
+          <div class="card-body">
+            <h6 class="card-title">Marketing for Entrepreneurs in 2022: Learn Digital Marketing</h6>
+            <p class="card-text">[UPDATED: NEW FREE COURSE] High Energy Digital Marketing, Growth-Hacking and Entrepreneurial Marketing Masterclass:</p>
+         
+            <a href="https://www.udemy.com/course/entrepreneur-marketing/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/4183724_c0b6_2.jpg"  class="card-img-top" alt="Javascript Essentials">
+          <div class="card-body">
+          <h6 class="card-title">WhatsApp Targeted Marketing</h6>
+            <p class="card-text">How To Use WhatsApp Business API Automation To Get The Best ROIs?</p>
+            <a href="https://www.udemy.com/course/whatsapp-targeted-marketing/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/1053706_c51c_7.jpg"  class="card-img-top" alt="C++ Tutorial for Complete Beginners">
+          <div class="card-body">
+          <h6 class="card-title">Big Data in Advertising - Explained in Plain English</h6>
+            <p class="card-text">A 30 min overview of what kind of data advertisings use and how that data is collected on your devices.</p>
+            <a href="https://www.udemy.com/course/big_data_in_advertising/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6 mb-4 mb-md-0">
+        <div class="card" >
+        <img src="udemy/1545852_ca94_2.jpg"  class="card-img-top" alt="Introduction To Python Programming">
+          <div class="card-body">
+          <h6 class="card-title">Inbound Sales</h6>
+            <p class="card-text">Become an inbound sales expert!</p>
+            <a href="https://www.udemy.com/course/inbound-sales/" class="btn btn-warning">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+
+include_once("course_category.php");?>
+
+
+ 
+<?php include('footer.php') ?>
+    
